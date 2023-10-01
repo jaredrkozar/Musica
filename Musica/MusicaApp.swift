e2f7484d789a8df2cbdb83de4ccd30813260b094
Jared Kozar <jared.rkozar@gmail.com>
2023-09-30 21:38:02 -0400

    Initial Commit

---
+++ Musica/MusicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicaApp.swift
+//  Musica
+//
+//  Created by Jared Kozar on 9/30/23.
+//
+
+import SwiftUI
+
+@main
+struct MusicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
