ffad50f2c4f54bb378ce6510040c98a3060ea824
Jared Kozar <jared.rkozar@gmail.com>
2023-10-13 13:59:56 -0400

    fixed audio playing issues

---
+++ Musica/MusicaIcon.swift
@@ -0,0 +1,18 @@
+//
+//  MusicaIcon.swift
+//  Musica
+//
+//  Created by Jared Kozar on 10/6/23.
+//
+
+import SwiftUI
+
+struct MusicaIcon: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MusicaIcon()
+}
